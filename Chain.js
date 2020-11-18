05af074ab85532ade83f33b433e5df68daef0b49
ishitabhavnani <71380504+ishitabhavnani@users.noreply.github.com>
2020-11-18 15:49:36 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,17 @@
+class Chain{
+    constructor(object1 , object2){
+      var options = {
+          bodyA: object1,
+          bodyB: object2,
+          length: 10,
+          stiffness:0.04
+      }
+      this.chain= Constraint.create(options)
+      World.add(world,this.chain);
+    }
+
+    display(){
+       
+        line(this.chain.bodyA.position.x ,this.chain.bodyA.position.y, this.chain.bodyB.position.x, this.chain.bodyB.position.y)
+    }
+}
